6c3b2c0e599cab43adfc61459b9003622f0a7540
successovo <successovo@gmail.com>
2022-09-08 15:19:51 -0700

    my first

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,6 @@
  * Return: Always 0 (success)
  */
 int main(void)
-{ 
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+{     puts("\"Programming is like building a multilingual puzzle");
+      return (0);
 }	
