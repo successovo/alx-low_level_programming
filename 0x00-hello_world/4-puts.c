c5ca6a67d2ef8e6bfdd481d96f2df9f5563a108f
successovo <successovo@gmail.com>
2022-09-08 14:47:40 -0700

    my first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry poin
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
-{ 	puts("Programming is like building a multilingual puzzle\n");
+{ 	puts("\"Programming is like building a multilingual puzzle");
+
 	return 0;
 }	
