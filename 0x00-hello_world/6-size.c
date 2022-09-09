c59786dacc72ac6d751250fa30892d7ff5c265b8
successovo <successovo@gmail.com>
2022-09-08 19:17:56 -0700

    my 6th task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - main block
+ * Return: 0
  */
-int main(void) 
-{	printf("char has %zu bits\n", CHAR_BIT * sizeof(char)); 
-	printf("short has %zu bits\n", CHAR_BIT * sizeof(short)); 
-	printf("int has %zu bits\n", CHAR_BIT * sizeof(int)); 
-	printf("long has %zu bits\n", CHAR_BIT * sizeof(long)); 
-	printf("long long has %zu bits\n", CHAR_BIT * sizeof(long long)); 
-	printf("float has %zu bits\n", CHAR_BIT * sizeof(float)); 
-	printf("double has %zu bits\n", CHAR_BIT * sizeof(double)); 
-	return (0); 
+int main(void)
+{
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
+	return (0);
 }
