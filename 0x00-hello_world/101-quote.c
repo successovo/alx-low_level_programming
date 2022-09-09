683811b83694b7beaac5c5120e505729ea16e443
successovo <successovo@gmail.com>
2022-09-08 19:58:37 -0700

    my taSK 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{	int i = 2015;
+	int n = 10;
+	int t = 19;
+
+	printf("and that piece of art is useful%c %c", '"', '-');
+	printf("Dora Korpar, %d%c%d%c%d\n", i, '-', n, '-', t);
+	return (1);
+}
